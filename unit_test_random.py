f87ea8e551dbfc266988acca2094cc25d9914332
U48P <tony.twumasi@gmail.com>
2019-11-29 08:26:33 +0100

    Kollar testerna

---
+++ unit_test_random.py
@@ -0,0 +1,44 @@
+import unittest
+import random
+import testclass
+
+
+
+class testRandint(unittest.TestCase):
+
+    def setUp(self):
+        global random
+        random = Random(23)
+
+    def test_getnumber(self):
+        result = testclass.getnumber(15,17)
+        a = 16 
+        self.assertEqual(result, a)
+
+    def test_length(self):
+        result = testclass.length(0,5)
+        self.assertEqual(result,5)    
+
+    def test_numbers(self):
+        result = testclass.numbers(5)
+        self.assertEqual(result,True)
+
+
+    def test_potentialnumbers(self):
+        result = testclass.potentialnumbers(1,6)
+        f = [1, 2, 3, 4, 5]
+        self.assertEqual(result,f)     
+        
+    # def test_add(self):
+    #     result = plus.add(1,2)
+    #     self.assertEqual(result,3)    
+
+    def setUp(self):
+        testclass.initialize()
+
+    def tearDown(self):
+        testclass.cleanUp()        
+        
+unittest.main()
+
+
